0c0854af5248e88336d177666327aa8e68a06f2f
Milad Parsa <m.paraks@gmail.com>
2024-10-11 18:57:31 +0330

    Delivery

--- src/main/java/com/mftplus/demo/model/entity/OrderItem.java
+++ src/main/java/com/mftplus/demo/model/entity/OrderItem.java
@@ -1,4 +1,30 @@
 package com.mftplus.demo.model.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+
+@Entity(name = "OrderItemEntity")
+@Table(name = "order_item_tbl")
 public class OrderItem {
+
+    @Id
+    @SequenceGenerator(name = "orderItemSeq", sequenceName = "orderItem_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "orderItemSeq")
+    private Long id;
+
+    @Column(name = "discount", length = 5, nullable = false)
+    private int discount;
+
+    @Column(name = "pure_amont", length = 5, nullable = false)
+    private int pure_amount;
+
 }
