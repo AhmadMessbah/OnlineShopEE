646c1c58db88f6a472707119e528e3bc3c488f41
Milad Parsa <m.paraks@gmail.com>
2024-10-11 18:45:27 +0330

    Delivery

--- src/main/java/com/mftplus/demo/model/entity/Order.java
+++ src/main/java/com/mftplus/demo/model/entity/Order.java
@@ -1,8 +1,11 @@
 package com.mftplus.demo.model.entity;
 
-import jakarta.persistence.Entity;
+import com.google.gson.Gson;
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
@@ -10,7 +13,34 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 
 
-@Entity(name = "")
+@Entity(name = "OrderEntity")
+@Table(name = "order_tbl")
 public class Order {
+    @Id
+    @SequenceGenerator(name = "orderSeq", sequenceName = "order_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "orderSeq")
+    private Long id;
+
+    private Person seller;
+    private Person customer;
+    private Delivery delivery;
+
+    @Column(name = "recive_time", nullable = false)
+    private LocalDateTime reciveTime;
+
+    @Column(name = "total_cost", nullable = false)
+    private Long totalCost;
+
+    @Column(name = "discount", nullable = false)
+    private Long discount;
+
+    @Column(name = "oder_Item", nullable = false)
+    private OrderItem orderItem;
+
+
+    @Override
+    public String toString(){
+        return new Gson().toJson(this);
+    };
 
 }
