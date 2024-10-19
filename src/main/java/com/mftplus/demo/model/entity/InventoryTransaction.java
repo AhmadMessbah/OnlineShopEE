435a18a9957e54a7e4c01bb457dd0bf3f33957f3
ZahraEbrahimi96 <ebrahimi.zahra1996@gmail.com>
2024-10-20 01:33:37 +0330

    zahra/update entity

--- src/main/java/com/mftplus/demo/model/entity/InventoryTransaction.java
+++ src/main/java/com/mftplus/demo/model/entity/InventoryTransaction.java
@@ -1,7 +1,10 @@
 package com.mftplus.demo.model.entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.Gson;
 import com.mftplus.demo.model.entity.enums.InventoryStatus;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,28 +15,39 @@ import lombok.experimental.SuperBuilder;
 @Setter
 @SuperBuilder
 
-@Entity(name = "InventoryTransaction")
+@Entity(name = "inventoryTransactionEtity")
 @Table(name = "Inventory-Transaction")
+@NamedQueries({
+        @NamedQuery(name="InventoryTransaction.findByInventoryId", query = "select oo from inventoryTransactionEtity oo where oo.inventory.id = :inventory"),
+        @NamedQuery(name ="InventoryTransaction.findByProductId", query = "select oo from inventoryTransactionEtity oo where oo.product.id = :product"),
+        @NamedQuery(name ="InventoryTransaction.findByOrderId", query = "select oo from inventoryTransactionEtity oo where oo.order.id = :order")
+})
 
-public class InventoryTransaction {
+public class InventoryTransaction extends Base {
 
+    @JsonProperty("Num")
     @Id
     @SequenceGenerator(name = "inventory_t_seq", sequenceName = "inventory_t_seq", initialValue = 1, allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @JsonIgnore
     @OneToOne
     @JoinColumn(name=("inventory"), foreignKey = @ForeignKey(name="fk-inventoryTrans_inventory"))
     private Inventory inventory;
 
+    @JsonIgnore
     @OneToOne
     @JoinColumn(name=("product"), foreignKey = @ForeignKey(name="fk-inventoryTrans_product"))
     private Product product;
 
+    @JsonIgnore
     @OneToOne
     @JoinColumn(name = ("order"), foreignKey= @ForeignKey(name ="fk-inventoryTrans-order"))
     private Order order;
 
+    @JsonProperty("Co")
+    @Pattern(regexp = "\\d", message = "Invalid Number")
     @Column(name = "count", length = 10)
     private int count;
 
