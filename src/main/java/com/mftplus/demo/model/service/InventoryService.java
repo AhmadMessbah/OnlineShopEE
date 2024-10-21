09cd039fbb379cf994f4335e02c6a014a3d2f175
ZahraEbrahimi96 <ebrahimi.zahra1996@gmail.com>
2024-10-21 13:44:25 +0330

    zahra/update services and api

--- src/main/java/com/mftplus/demo/model/service/InventoryService.java
+++ src/main/java/com/mftplus/demo/model/service/InventoryService.java
@@ -1,65 +1,69 @@
-//package com.mftplus.demo.model.service;
-//import com.mftplus.demo.model.entity.Inventory;
-//import com.mftplus.demo.model.repository.CrudRepository;
-//
-//import java.util.HashMap;
-//import java.util.List;
-//
-//public class InventoryService implements Service<Inventory, Long> {
-//
-//
-//    @Override
-//    public void save(Inventory inventory) throws Exception {
-//        try(CrudRepository<Inventory, Long> repository = new CrudRepository<>()){
-//            repository.save(inventory);
-//        }
-//    }
-//
-//    @Override
-//    public void edit(Inventory inventory) throws Exception {
-//        try(CrudRepository<Inventory, Long> repository = new CrudRepository<>()){
-//            repository.edit(inventory);}
-//    }
-//
-//    @Override
-//    public void remove(Long id) throws Exception {
-//        try(CrudRepository<Inventory, Long> repository = new CrudRepository<>()){
-//            repository.remove(id, Inventory.class);}
-//    }
-//
-//    @Override
-//    public Inventory findById(Long id) throws Exception {
-//        try(CrudRepository<Inventory, Long> repository = new CrudRepository<>()){
-//          return repository.findById(id, Inventory.class);}
-//    }
-//
-//    @Override
-//    public List<Inventory> findAll() throws Exception {
-//        try(CrudRepository<Inventory, Long> repository = new CrudRepository<>()){
-//            return repository.findAll(Inventory.class);}
-//    }
-//
-//    public List<Inventory> findByTitle (String title) throws Exception {
-//        try(CrudRepository<Inventory, Long> repository = new CrudRepository<>()){
-//            HashMap<String, Object> params = new HashMap<>();
-//            params.put("title",title+"%");
-//            return repository.findBy("Inventory.findByTitle", params, Inventory.class);
-//        }
-//    }
-//
-//    public List<Inventory> findByAddress (String address) throws Exception {
-//        try(CrudRepository<Inventory, Long> repository = new CrudRepository<>()){
-//            HashMap<String, Object> params = new HashMap<>();
-//            params.put("address",address+"%");
-//            return repository.findBy("Inventory.findByAddress", params, Inventory.class);
-//        }
-//    }
-//
-//    public List<Inventory> findByPhone (String phone) throws Exception {
-//        try(CrudRepository<Inventory, Long> repository = new CrudRepository<>()){
-//            HashMap<String, Object> params = new HashMap<>();
-//            params.put("phone",phone+"%");
-//            return repository.findBy("Inventory.findByPhone", params, Inventory.class);
-//        }
-//    }
-//}
+package com.mftplus.demo.model.service;
+
+import com.mftplus.demo.model.entity.Inventory;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+
+@ApplicationScoped
+@Slf4j
+public class InventoryService{
+
+    @PersistenceContext(unitName = "mft")
+    private EntityManager entityManager;
+
+    @Transactional
+    public void save(Inventory inventory){
+        entityManager.persist(inventory);
+        }
+
+    @Transactional
+    public void edit(Inventory inventory){
+        entityManager.merge(inventory);
+    }
+
+    @Transactional
+    public void remove(Long id){
+       Inventory inventory = entityManager.find(Inventory.class, id);
+       entityManager.remove(inventory);
+    }
+
+    @Transactional
+    public Inventory findById(Long id){
+        return entityManager.find(Inventory.class, id);
+    }
+
+    @Transactional
+    public List<Inventory> findAll(){
+        Query query = entityManager.createQuery("select oo from inventoryEntity oo",Inventory.class);
+        return query.getResultList();
+    }
+
+    @Transactional
+    public List<Inventory> findByTitle(String title) {
+        Query query = entityManager.createQuery("select oo from inventoryEntity oo where oo.title = :title", Inventory.class);
+        query.setParameter("title", title);
+        return query.getResultList();
+    }
+
+    public List<Inventory> findByAddress (String address){
+        Query query = entityManager.createQuery("select oo from inventoryEntity oo where oo.address = :address", Inventory.class );
+        query.setParameter("address", address);
+
+        return query.getResultList();
+    }
+
+    public List<Inventory> findByPhone (String phone){
+        Query query = entityManager.createQuery("select oo from inventoryEntity oo where oo.phone = :phone", Inventory.class );
+        query.setParameter("phone", phone);
+
+        return query.getResultList();
+    }
+
+}
