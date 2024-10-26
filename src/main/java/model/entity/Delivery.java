1a6734585b869f42f33ea36c7254eea94ae7092c
Milad Parsa <m.paraks@gmail.com>
2024-10-26 16:11:14 +0330

    2.8

--- src/main/java/model/entity/Delivery.java
+++ src/main/java/model/entity/Delivery.java
@@ -1,12 +1,39 @@
 package model.entity;
 
-import jakarta.transaction.Status;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import model.entity.enums.DeliveryStatus;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
 
 public class Delivery {
+
+    @Id
+    @SequenceGenerator(name = "deliverySeq", sequenceName = "delivery_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "deliverySeq")
     private Long id;
-//    private enum transportation_modes;
-//    private Status status;
-//    private Person customer;
-//    private Person shipper;
+
+    private String orderId;
+    private String deliveryAddress;
+    private String recipientName;
+    private String phoneNumber;
+    private LocalDateTime shippedDate;
+    private LocalDateTime deliveredDate;
+    private String deliveryMethod;
+    private String trackingNumber;
+    private String carrier;
+    private double shippingCost;
+    private DeliveryStatus deliveryStatus;
 
 }
---
+++ src/main/java/model/entity/enums/DeliveryStatus.java
@@ -0,0 +1,11 @@
+package model.entity.enums;
+
+public enum DeliveryStatus {
+    pending,
+    shipped,
+    inTransit,
+    outOfDelivery,
+    delivered,
+    canceled,
+    returned
+}
