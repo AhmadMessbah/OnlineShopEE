1f6a85f5ecb827e2074342c41c7d53d98406c5e9
Asus <hodaghiasi1363@gmail.com>
2024-11-02 14:28:27 +0330

    ProductService.update new

--- src/main/java/com/mftplus/demo/model/entity/ProductGroup.java
+++ src/main/java/com/mftplus/demo/model/entity/ProductGroup.java
@@ -1,36 +1,44 @@
 package com.mftplus.demo.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 @NoArgsConstructor
 @Getter
 @Setter
+@SuperBuilder
 
 @Entity(name="productGroupEntity")
 @Table(name = "productGroup_tbl")
-@SuperBuilder
 
 
 
-public class ProductGroup {
+public class ProductGroup extends Base{
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    @Column(name = "name", nullable = false)
-    private String name;
-    @OneToMany
-    @Column(name = "productGroup-child")
-    private List<ProductGroup> child;
+    @SequenceGenerator(name = "productGroupSeq", sequenceName = "productGroup_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE , generator = "productGroupSeq" )
+    private Long id;
 
-    @OneToOne
+    @Column(name = "name", nullable = false,length = 30)
+    @Pattern(regexp = "^[a-zA-Z]{3,30}$",message = "invalid name!")
+    private String name;
+
+    @Column(name = "productGroup-child")
+    private String child;
+
     @Column(name="productGroup-parent")
-    private  ProductGroup parent;
+    private  String parent;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST},fetch = FetchType.EAGER)
+    private List<Product> productList = new ArrayList<>();
 
 
 }
