661c71cbae6cb99af442a50b72e65c1a9d6ae81b
sana center <azimi1376@gmail.com>
2024-11-04 14:20:08 +0330

    Mobina Updated To JTA.

--- src/main/java/com/mftplus/demo/model/service/Service.java
+++ src/main/java/com/mftplus/demo/model/service/Service.java
@@ -3,9 +3,9 @@ package com.mftplus.demo.model.service;
 import java.util.List;
 
 public interface Service <T,I>{
-    public void save(T t)throws Exception;
-    public void edit(T t)throws Exception;
-    public void remove(I id)throws Exception;
-    public T findById(I id)throws Exception;
-    public List<T> findAll()throws Exception;
+    public void save(T t);
+    public void edit(T t);
+    public void remove(I id);
+    public T findById(I id);
+    public List<T> findAll();
 }
