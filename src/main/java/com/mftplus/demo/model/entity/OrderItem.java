397de3283cb450d62827547645f7013c48d5a8b0
Milad Parsa <97868926+Miladparaks@users.noreply.github.com>
2024-11-09 20:13:09 +0330

    Update OrderItem.java

--- src/main/java/com/mftplus/demo/model/entity/OrderItem.java
+++ src/main/java/com/mftplus/demo/model/entity/OrderItem.java
@@ -2,29 +2,46 @@ package com.mftplus.demo.model.entity;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
 
 @NoArgsConstructor
 @Getter
 @Setter
 @SuperBuilder
+@ToString
 
 @Entity(name = "OrderItemEntity")
 @Table(name = "order_item_tbl")
 public class OrderItem {
-
-    @Id
-    @SequenceGenerator(name = "orderItemSeq", sequenceName = "orderItem_seq", allocationSize = 1)
+  @Id
+    @SequenceGenerator(name = "orderItemSeq", sequenceName = "order_item_seq", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "orderItemSeq")
+    @Column(name = "order_Id")
     private Long id;
 
-    @Column(name = "discount", length = 5, nullable = false)
-    private int discount;
+    @Column(name = "product_id")
+    private int productId;
 
-    @Column(name = "pure_amont", length = 5, nullable = false)
-    private int pure_amount;
+    @Column(name = "product_quantity")
+    private double quantity;
+
+    @Column(name = "unit_price")
+    private long unitPrice;
+
+    @Transient
+    private double totalPrice;
+
+    public double getTotalPrice() {
+        totalPrice = quantity * unitPrice;
+        return totalPrice;
+    }
 
 }
