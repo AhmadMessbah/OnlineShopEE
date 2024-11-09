526e69313e8ce822816479b53e3fb9dcfda0d053
Milad Parsa <97868926+Miladparaks@users.noreply.github.com>
2024-11-09 20:28:52 +0330

    Update Delivery.java

--- src/main/java/com/mftplus/demo/model/entity/Delivery.java
+++ src/main/java/com/mftplus/demo/model/entity/Delivery.java
@@ -1,43 +1,49 @@
-//package com.mftplus.demo.model.entity;
-//
-//
-//import com.google.gson.Gson;
-//import jakarta.persistence.*;
-//import lombok.*;
-//import lombok.experimental.SuperBuilder;
-//
-//@NoArgsConstructor
-//@Getter
-//@Setter
-//@SuperBuilder
-//
-//
-//@Entity(name= "DeliveryEntity")
-//public class Delivery {
-//    @Id
-//    @SequenceGenerator(name = "deliverySeq", sequenceName = "delivery_seq", allocationSize = 1)
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "deliverySeq")
-//    private Long id;
-//
-//
-//    //TODO: mehdi please check .
-//    private enum transportation_modes;
-//    private enum status;
-//
-//    @OneToOne
-//    @Column (name = "Delivery_customer", length = 20, nullable = false)
-//    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-//
-//    private Person customer;
-//
-//    @OneToOne
-//    @Column (name = "Shipper", length = 20, nullable = false)
-//    private Person Shipper;
-//
-//    @Override
-//    public String toString() {
-//        return new Gson().toJson(this);
-//    }
-//
-//
-//}
+package com.mftplus.demo.model.entity;
+import com.mftplus.demo.model.entity.enums.DeliveryMethod;
+import com.mftplus.demo.model.entity.enums.DeliveryStatus;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@SuperBuilder
+
+@Entity(name= "DeliveryEntity")
+@Table(name = "delivery_tbl")
+public class Delivery {
+
+    @Id
+    @SequenceGenerator(name = "deliverySeq", sequenceName = "delivery_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "deliverySeq")
+    private Long id;
+
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Column(name = "delivery_address", nullable = false)
+    private String deliveryAddress;
+    @Column(name = "tracking_number", nullable = false)
+    private String trackingNumber;
+    @Column(name = "carrier")
+    private String carrier;
+
+
+    private double shippingCost;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus deliveryStatus;
+    private DeliveryMethod deliveryMethod;
+
+    private String phoneNumber;
+    private LocalDateTime shippedDate;
+    private LocalDateTime deliveredDate;
+}
