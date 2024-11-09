76723f412a58918cc3893366eee4687547136e29
Asus <hodaghiasi1363@gmail.com>
2024-11-09 16:47:24 +0330

    hoda,update ProductService and ProductGroupService and api

---
+++ src/main/java/com/mftplus/demo/controller/api/TestApiProduct.java
@@ -0,0 +1,29 @@
+package com.mftplus.demo.controller.api;
+
+import com.mftplus.demo.model.entity.Product;
+import com.mftplus.demo.model.service.ProductService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import lombok.extern.slf4j.Slf4j;
+
+@Path("/test")
+@Slf4j
+public class TestApiProduct {
+
+    @Inject
+    private ProductService productService;
+
+    @GET
+
+    public String test() {
+        log.info("test loaded++");
+
+        Product product = Product.builder().name("shampoo").price(170F).code(111L).build();
+        productService.save(product);
+        return product.toString();
+
+    }
+
+
+}
