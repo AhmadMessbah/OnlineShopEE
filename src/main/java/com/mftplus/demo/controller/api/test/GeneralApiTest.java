1a5e259c583a7a45a2adb7169a8f64975e2c9e16
ali_he96 <alihesarakitrade@gmail.com>
2024-11-18 19:38:52 +0330

    ali: update GeneralApiTest

--- src/main/java/com/mftplus/demo/controller/api/test/GeneralApiTest.java
+++ src/main/java/com/mftplus/demo/controller/api/test/GeneralApiTest.java
@@ -6,6 +6,7 @@ import com.mftplus.demo.model.entity.Transaction;
 import com.mftplus.demo.model.service.BankService;
 import com.mftplus.demo.model.service.PaymentService;
 import com.mftplus.demo.model.service.TransactionService;
+import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
@@ -27,10 +28,9 @@ public class GeneralApiTest {
     @Inject
     private TransactionService transactionService;
 
-    @GET
-    @Path("/test_all")
-    public String testAll() {
-        log.info("Testing all APIs");
+    @PostConstruct
+    public void initialize() {
+        log.info("Initializing default data...");
 
         // Test Bank API
         Bank bank = Bank.builder()
@@ -39,7 +39,7 @@ public class GeneralApiTest {
                 .branchCode(111L)
                 .build();
         bankService.save(bank);
-        log.info("Bank created: {}", bank);
+        log.info("Default bank created: {}", bank);
 
         // Test Payment API
         Payment payment = Payment.builder()
@@ -48,7 +48,7 @@ public class GeneralApiTest {
                 .description("General Test Payment")
                 .build();
         paymentService.save(payment);
-        log.info("Payment created: {}", payment);
+        log.info("Default payment created: {}", payment);
 
         // Test Transaction API
         Transaction transaction = Transaction.builder()
@@ -56,7 +56,13 @@ public class GeneralApiTest {
                 .date(LocalDate.now())
                 .build();
         transactionService.save(transaction);
-        log.info("Transaction created: {}", transaction);
+        log.info("Default transaction created: {}", transaction);
+    }
+
+    @GET
+    @Path("/test_all")
+    public String testAll() {
+        log.info("Testing all APIs");
 
         return "All APIs tested successfully!";
     }
@@ -65,21 +71,36 @@ public class GeneralApiTest {
     @Path("/bank/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getBankById(@PathParam("id") Long id) {
-        return Response.ok(bankService.findById(id)).build();
+        Bank bank = bankService.findById(id);
+        if (bank == null) {
+            log.error("Bank with id {} not found", id);
+            return Response.status(Response.Status.NOT_FOUND).entity("Bank not found").build();
+        }
+        return Response.ok(bank).build();
     }
 
     @GET
     @Path("/payment/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getPaymentById(@PathParam("id") Long id) {
-        return Response.ok(paymentService.findById(id)).build();
+        Payment payment = paymentService.findById(id);
+        if (payment == null) {
+            log.error("Payment with id {} not found", id);
+            return Response.status(Response.Status.NOT_FOUND).entity("Payment not found").build();
+        }
+        return Response.ok(payment).build();
     }
 
     @GET
     @Path("/transaction/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getTransactionById(@PathParam("id") Long id) {
-        return Response.ok(transactionService.findById(id)).build();
+        Transaction transaction = transactionService.findById(id);
+        if (transaction == null) {
+            log.error("Transaction with id {} not found", id);
+            return Response.status(Response.Status.NOT_FOUND).entity("Transaction not found").build();
+        }
+        return Response.ok(transaction).build();
     }
 
     @PUT
