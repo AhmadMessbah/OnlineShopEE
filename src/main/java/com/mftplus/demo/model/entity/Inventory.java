094ef6a2162a58ec62b0346cedc926764dbb7c3b
ZahraEbrahimi96 <ebrahimi.zahra1996@gmail.com>
2024-11-18 19:48:12 +0330

    entities updated

--- src/main/java/com/mftplus/demo/model/entity/Inventory.java
+++ src/main/java/com/mftplus/demo/model/entity/Inventory.java
@@ -1,10 +1,12 @@
 package com.mftplus.demo.model.entity;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
+
 import lombok.experimental.SuperBuilder;
 
 
@@ -12,7 +14,7 @@ import lombok.experimental.SuperBuilder;
 @Getter
 @Setter
 @SuperBuilder
-@ToString
+
 
 @Entity(name = "inventoryEntity")
 @Table(name = "inventory_table")
@@ -22,26 +24,34 @@ public class Inventory extends Base {
     @Id
     @SequenceGenerator(name = "inventory_seq", sequenceName = ("inventory_seq"), allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "inventory_seq")
+    @JsonProperty("ردیف")
     private Long id;
 
-  //  @Pattern(regexp = "^[a-zA-Z\\s]{3,30}$",message = "Invalid Name")
-    @Column(name = "title", length = 30)//, nullable = false
+  //  @Pattern(regexp = "^[a-zA-Z\\s]{3,30}$",message = "نام اشتباه است!!")
+    @Column(name = "title", length = 30)
+    @NotNull(message = "نام انبار را وارد نکرده اید!!")
+    @JsonProperty("نام")
     private String title;
 
-  //  @Pattern(regexp = "^[a-zA-Z0-9]{3,300}$",message = "Invalid Address")
-    @Column(name = "address", length = 200)//, nullable = false
+  //  @Pattern(regexp = "^[a-zA-Z0-9]{3,300}$",message = "آدرس اشتباه است!!")
+    @Column(name = "address", length = 200)
+    @NotNull(message = "آدرس را وارد نکرده اید!!")
+    @JsonProperty("آدرس")
     private String address;
 
-  //  @Pattern(regexp = "[\\d]{11}",message = "Invalid Phone Number")
-    @Column(name = "phone", length = 14)//, nullable = false
+  //  @Pattern(regexp = "[\\d]{11}",message = "شماره تماس اشتباه است!!")
+    @Column(name = "phone", length = 14)
+    @NotNull(message = "شماره تماس را وارد نکرده اید!!")
+    @JsonProperty("شماره تماس")
     private String phone;
 
     @ManyToOne(cascade = CascadeType.PERSIST , fetch = FetchType.EAGER)
-    @JoinColumn(name = "I_transaction")
-    private InventoryTransaction inventoryTransaction;
+    @JoinColumn(name = "inventory_product", foreignKey = @ForeignKey(name="fk-inventory_product"))
+    @JsonProperty("کالا های انبار")
+    private InventoryProduct inventoryProduct;
 
-//    @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
-//    @JoinColumn(name = "InventoryTransaction")
-//    private List<InventoryTransaction> inventoryTransaction;
+
+
+
 
 }
