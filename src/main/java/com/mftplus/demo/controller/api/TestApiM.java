2c24d776e17dc7c40c8e1a1104849bc6ba0cc631
sana center <azimi1376@gmail.com>
2024-12-23 19:41:47 +0330

    Mobina.updated testApi

--- src/main/java/com/mftplus/demo/controller/api/TestApiM.java
+++ src/main/java/com/mftplus/demo/controller/api/TestApiM.java
@@ -1,4 +1,5 @@
 package com.mftplus.demo.controller.api;
+
 import com.mftplus.demo.model.entity.*;
 import com.mftplus.demo.model.service.PersonService;
 import com.mftplus.demo.model.service.TicketService;
@@ -14,63 +15,32 @@ import java.util.Set;
 @Loggable
 public class TestApiM {
 
+//    @Inject
+//    private PersonService personService;
     @Inject
-//    private MessageService messageService;
-    private PersonService personService;
-//    private TicketService ticketService;
-//    private UserService userService;
+    private TicketService ticketService;
 
-    @GET
-//    @ResponseMaker(authority = "SAVE_USER")
-    public String test() {
-//        Person person=Person.builder().name("aas").family("ass").build();
+    @GET public String test() {
 
-
-//        User user = User.builder().username("mobi").build();
-//        TicketGroup t = new TicketGroup();
-//        userService.save(user);
 //        while (t.getParent() != null) {
 //            System.out.println("ticket group parent: " + t.getParent().getName());
 //        }
-//        return user.toString();
-
-//        Message message=Message.builder().text("helloooo").build();
-//        messageService.save(message);
-//        return message.toString();
-
-//        User user=User.builder().username("mmm").build();
-//        userService.save(user);
-//        return user.toString();
-//        Person person=Person.builder().name("mobina").build();
-//        personService.save(person);
-//        return person.toString();
-
-                //DO
-//        TicketGroup child=new TicketGroup();
-//        TicketGroup parent=new TicketGroup();
-//        child.setName("digital");
-//        parent.setName("electronic");
-//        TicketGroup ticketGroup=TicketGroup.builder().name("mobile").parent(parent).build();
 
         Permission permission = Permission.builder().permissionName("get").build();
         Role role = Role.builder().permissionSet(Set.of(permission)).roleName("admin").build();
-        User user=User.builder().username("eeee").password("234").roleList(List.of(role)).build();
-        Person person=Person.builder().name("aas").family("ass").user(user).build();
+        User user = User.builder().username("eeee").password("234").roleList(List.of(role)).build();
+        Person person = Person.builder().name("aas").family("ass").user(user).build();
+        Message message = Message.builder().text("helloooo").title("say-hello").user(user).build();
+        TicketGroup child = new TicketGroup();
+        TicketGroup parent = new TicketGroup();
+        child.setName("digital");
+        parent.setName("electronic");
+        TicketGroup ticketGroup = TicketGroup.builder().name("mobile").parent(parent).build();
+        Ticket ticket = Ticket.builder().messages(List.of(message)).ticketGroup(ticketGroup).title("buy").user(user).responseType("delivered").text("this is your order").build();
 
-//        Message message = Message.builder().user(user).title("security requirement").text("please check").dateTime("2024.01.23").build();
-//        Ticket ticket=Ticket.builder().messages(List.of(message)).ticketGroup(ticketGroup).title("buy").text("this is your order").build();
-
-//        TicketGroup t =new TicketGroup();
-//        while (t.getParent() !=null){
-//            System.out.println(parent.getChildList());
-//
-//        }
-//        ticketService.save(ticket);
-//        return ticket.toString();
-//        userService.save(user);
-//        return user.toString();
-        personService.save(person);
-        return person.toString();
+//        personService.save(person);
+        ticketService.save(ticket);
+        return ticket.toString();
 
     }
 }
